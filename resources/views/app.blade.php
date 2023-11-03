4edef9825df926c25f8672868582da23e68d83a2
SaiWinOo <saiwinoo52@gmail.com>
2023-11-03 12:48:31 +0630

    done adingg meta tag

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,10 +3,40 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Laravel</title>
+    <link rel="shortcut icon" href="https://i.postimg.cc/L55hXGQ8/Asset-14-300x-1.jpg" type="image/x-icon">
     @viteReactRefresh
     @vite(['resources/css/app.css', 'resources/js/app.jsx'])
     @inertiaHead
+
+    <!--    meta tag -->
+
+    <meta name="title" content="Pika Sharing">
+    <meta name="description" content="This site is to provide people with proven how-tos, free some minds, and document Pika's evolution.">
+    <meta name="keywords" content="Pika , Pika Sharing, Pika English">
+    <meta name="robots" content="index, follow">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="language" content="English">
+
+
+    <!-- Primary Meta Tags -->
+    <title>Pika Sharing</title>
+    <meta name="title" content="Pika Sharing" />
+    <meta name="description" content="This site is to provide people with proven how-tos, free some minds, and document Pika's evolution." />
+
+    <!-- Open Graph / Facebook -->
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="https://metatags.io/" />
+    <meta property="og:title" content="Pika Sharing" />
+    <meta property="og:description" content="This site is to provide people with proven how-tos, free some minds, and document Pika's evolution." />
+    <meta property="og:image" content="https://i.postimg.cc/L55hXGQ8/Asset-14-300x-1.jpg" />
+
+    <!-- Twitter -->
+    <meta property="twitter:card" content="summary_large_image" />
+    <meta property="twitter:url" content="https://metatags.io/" />
+    <meta property="twitter:title" content="Pika Sharing" />
+    <meta property="twitter:description" content="This site is to provide people with proven how-tos, free some minds, and document Pika's evolution." />
+    <meta property="twitter:image" content="https://i.postimg.cc/L55hXGQ8/Asset-14-300x-1.jpg" />
+
 </head>
 <body>
 @inertia
